3708d61b507b0144981454276b7d8905916e9ec1
ta86494 <2021wa86494@wilp.bits-pilani.ac.in>
2025-07-08 21:18:45 +0530

    Delete rmi directory

--- rmi/BillingInterface.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BillingInterface extends Remote {
-    String generateInvoice(String itemDetails) throws RemoteException;
-}
